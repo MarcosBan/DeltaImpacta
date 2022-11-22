0dadc22212b6496446d5ac740a36f03a2fbd471b
cainat <52249215+cainat@users.noreply.github.com>
2022-11-22 13:29:28 -0300

    Atualizando conexão com o novo BD

--- python/Usuario/usuario/database.py
+++ python/Usuario/usuario/database.py
@@ -1,14 +1,14 @@
 from peewee import *
 
 #HOST = "delta-mysqlserver.mysql.database.azure.com"
-HOST = "remotemysql.com"
+HOST = "35.198.23.78"
 #USER = "DeltaAdmin@delta-mysqlserver"
-USER = "9gh33Y1Tqh"
+USER = "root"
 #PASSWORD = "D3lt4T3e4m!"
-PASSWORD = "HVcIVr35dF" #"e4EyAPShpq"
+PASSWORD = "root" #"e4EyAPShpq"
 PORT = "3306"
 #DATABASE = 'budgettracker'
-DATABASE = "9gh33Y1Tqh" #xWPAAVgWh1"
+DATABASE = "deltabd-agoravai" #xWPAAVgWh1"
 
 conn = MySQLDatabase(
     DATABASE, user=USER,

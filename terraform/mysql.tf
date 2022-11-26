2e0e411ec2b26e50b480b64edfa1630c980305ea
Marcos Silva <marcos.mps10@gmail.com>
2022-11-26 16:47:18 -0300

    Update mysql.tf

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -3,8 +3,8 @@ resource "azurerm_mysql_server" "deltaserver" {
   location            = azurerm_resource_group.DeltaResources.location
   resource_group_name = azurerm_resource_group.DeltaResources.name
 
-  administrator_login          = "DeltaAdmin"
-  administrator_login_password = "D3lt4T3e4m!"
+  administrator_login          = ""
+  administrator_login_password = "!"
 
   sku_name   = "GP_Gen5_2"
   storage_mb = 5120
